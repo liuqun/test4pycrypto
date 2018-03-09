e0316faff27c13e710e22c7634330550aae38cf9
Liu Qun (liuqun) <qunliu@zyhx-group.com>
2018-03-09 18:31:01 +0800

    An AES test sample

---
+++ aes_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*-coding:utf-8 -*-
+from __future__ import print_function
+import os
+from cryptography.hazmat.backends import openssl
+from cryptography.hazmat.primitives.ciphers import Cipher, modes
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from cryptography.hazmat.primitives.padding import PKCS7
+
+
+def GeneratePseudoRandomBits128():
+    N_BITS = 128
+    key = b'1' * (N_BITS / 8)  # if DEBUG: iv = bytes(bytearray(N_BITS / 8))
+    return key
+
+
+if "__main__" == __name__:
+    print('Pseudo random key:')
+    key = GeneratePseudoRandomBits128()
+    print(key.encode('hex'))
+    iv = os.urandom(16)  # if DEBUG: iv = bytes(bytearray(AES.block_size/8))
+    print('Pseudo random IV:')
+    print(iv.encode('hex'))
+    #######################################
+    plain_text_utf8 = '''Hello world! This is my first secret message. 原始明文字符串汉字按UTF-8编码'''
+    print('Plain text:')
+    print(plain_text_utf8.decode('utf-8'))
+    print('Plain text in hex:')
+    print(plain_text_utf8.encode('hex'))
+    padder = PKCS7(AES.block_size).padder()
+    padded_data = padder.update(plain_text_utf8) + padder.finalize()
+    print('Padded data:')
+    print(padded_data.encode('hex'))
+    #######################################
+    my_aes128_cipher = Cipher(AES(key), modes.CBC(iv), backend=openssl.backend)
+    encryptor = my_aes128_cipher.encryptor()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+    print('Encrypted text:')
+    print(ct.encode('hex'))
+    #######################################
+    decryptor = my_aes128_cipher.decryptor()
+    decrypted_text_utf8 = decryptor.update(ct) + decryptor.finalize()
+    print('Decrypted text:')
+    print(decrypted_text_utf8.decode('utf-8'))
