9e5cb4360994f12b9b16b66e880fd742964823b4
Liu Qun (liuqun) <517067180@qq.com>
2018-04-26 13:02:13 +0800

    变更多处细节代码,以便支持Python 3.x
    注: 改动包括
    1. Python2.7.x open(name='文件名') --> Python3.x open(file='文件名')
    2. Python3.x不再支持encode('hex'). 必须改用 binascii.hexlify()
    3. Python3.x Unicode字符串与UTF-8字节流之间需要手动转换

--- aes_test.py
+++ aes_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from __future__ import print_function
 import os
@@ -13,37 +13,52 @@ def generate_pseudo_random_bits(n_bits):
     return b'1' * int(n_bits / 8)
 
 
+import sys
+
+
+def utf8_from_str(s):
+    assert isinstance(s, str)
+    if sys.hexversion >= 0x03000000:  # Python version 3.x
+        utf8 = s.encode('utf-8')
+    else:
+        utf8 = bytes(s)
+    return utf8
+
+
 if "__main__" == __name__:
+    from binascii import hexlify
+
     print('Pseudo random key:')
     key = generate_pseudo_random_bits(128)
-    print(key.encode('hex'))
+    print(hexlify(key).decode())
     iv = os.urandom(16)  # if DEBUG: iv = bytes(bytearray(AES.block_size/8))
     print('Pseudo random IV:')
-    print(iv.encode('hex'))
+    print(hexlify(iv).decode())
     #######################################
-    plain_text_utf8 = '''Hello world! This is my first secret message. 原始明文字符串汉字按UTF-8编码'''
+    plain_test_str = '''Hello world! This is my first secret message. 原始明文字符串汉字按UTF-8编码'''
+    plain_text_utf8 = utf8_from_str(plain_test_str)
     print('Plain text:')
     print(plain_text_utf8.decode('utf-8'))
     print('Plain text in hex:')
-    print(plain_text_utf8.encode('hex'))
+    print(hexlify(plain_text_utf8).decode())
     padder = PKCS7(AES.block_size).padder()
     padded_data = padder.update(plain_text_utf8) + padder.finalize()
     print('Padded data:')
-    print(padded_data.encode('hex'))
+    print(hexlify(padded_data).decode())
     #######################################
     my_aes128_cipher = Cipher(AES(key), modes.CBC(iv), backend=openssl.backend)
     encryptor = my_aes128_cipher.encryptor()
     ct = encryptor.update(padded_data) + encryptor.finalize()
     print('Encrypted text:')
-    print(ct.encode('hex'))
+    print(hexlify(ct).decode())
     #######################################
     decryptor = my_aes128_cipher.decryptor()
     decrypted_data_utf8 = decryptor.update(ct) + decryptor.finalize()
     print('Decrypted data in hex:')
-    print(decrypted_data_utf8.encode('hex'))
+    print(hexlify(decrypted_data_utf8).decode())
     unpadder = PKCS7(AES.block_size).unpadder()
     original_message_utf8 = unpadder.update(decrypted_data_utf8) + unpadder.finalize()
     print('Original message in hex:')
-    print(original_message_utf8.encode('hex'))
+    print(hexlify(original_message_utf8).decode())
     print('Original message:')
     print(original_message_utf8.decode('utf-8'))
--- hash_test.py
+++ hash_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*-coding:utf-8 -*-
 from __future__ import print_function
 from cryptography.hazmat.backends import default_backend
@@ -12,6 +12,7 @@ def create_raw_data_file(filename):
 
 
 if "__main__" == __name__:
+    from binascii import hexlify
     import os.path
 
     filename = 'data.raw'
@@ -21,7 +22,7 @@ if "__main__" == __name__:
     # 测试开始
     hash_alg = hashes.SHA256()
     hasher = hashes.Hash(algorithm=hash_alg, backend=default_backend())
-    with open(name=filename, mode='rb') as raw_data_file:
+    with open(filename, mode='rb') as raw_data_file:
         while True:
             block = raw_data_file.read(hash_alg.block_size)
             # 注释: 只有进行异步 IO 读写时才可能遇到 block 为 None 的情况
@@ -31,6 +32,6 @@ if "__main__" == __name__:
                 break
             hasher.update(block)
     raw_digest = hasher.finalize()
-    printable_digest = raw_digest.encode('hex')
+    printable_digest_str = hexlify(raw_digest).decode()
     print('SHA256 result:')
-    print(printable_digest.upper())
+    print(printable_digest_str.upper())
